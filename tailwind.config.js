ab9415ba325f799446baeea9a2fd7b89ee83c147
Abdi Setiawan <2200016103@WEBMAIL.UAD.AC.ID>
2025-08-06 17:34:15 +0700

    change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
-module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      "./index.html",
+          "./src/**/*.{js,ts,jsx,tsx}",
+            ],
+              theme: {
+                  extend: {
+                        fontFamily: {
+                                'sans': ['Inter', 'ui-sans-serif', 'system-ui'],
+                                        'heading': ['Montserrat', 'ui-sans-serif', 'system-ui'],
+                                                'body': ['Poppins', 'ui-sans-serif', 'system-ui'],
+                                                      },
+                                                          },
+                                                            },
+                                                              plugins: [],
+                                                              } */
