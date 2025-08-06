dbcf4828c75986f8e5129691323f79616683a6ef
Abdi Setiawan <2200016103@WEBMAIL.UAD.AC.ID>
2025-08-06 18:22:22 +0700

    tidy up syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-      "./index.html",
-          "./src/**/*.{js,ts,jsx,tsx}",
-            ],
-              theme: {
-                  extend: {
-                        fontFamily: {
-                                'sans': ['Inter', 'ui-sans-serif', 'system-ui'],
-                                        'heading': ['Montserrat', 'ui-sans-serif', 'system-ui'],
-                                                'body': ['Poppins', 'ui-sans-serif', 'system-ui'],
-                                                      },
-                                                          },
-                                                            },
-                                                              plugins: [],
-                                                              } */
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'ui-sans-serif', 'system-ui'],
+        'heading': ['Montserrat', 'ui-sans-serif', 'system-ui'],
+        'body': ['Poppins', 'ui-sans-serif', 'system-ui'],
+      },
+    },
+  },
+  plugins: [],
+}
